f753c2ad39e3bc0f2f1579310817af8cf4ffe4ee
Gerke Geurts <ggeurts@geurtsrus.com>
2018-09-17 12:28:57 +0200

    Re-enable large join test

--- Rhino.Etl.Tests/LoadTest/LoadTestJoinsFixture.cs
+++ Rhino.Etl.Tests/LoadTest/LoadTestJoinsFixture.cs
@@ -5,8 +5,8 @@ namespace Rhino.Etl.Tests.LoadTest
  
     public class LoadTestJoinsFixture
     {
-        [Fact(Skip = "It depends too much of what the machine is doing and how powerful it is")]
-        public void CanDoLargeJoinsefficently()
+        [Fact]
+        public void CanDoLargeJoinsEfficiently()
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             using(Join_250_000_UsersWithMostlyFallingOut proc = new Join_250_000_UsersWithMostlyFallingOut())
